66a9e4465b4e284754422dbc239a1dcecce1c011
rumeysametin <rumeysametin1166@gmail.com>
2022-07-18 16:35:49 +0300

    modals

---
+++ main.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById('simplemodel');
+var modelbutton = document.getElementById('modelbutton');
+var closebutton = document.getElementsByClassName('closebutton')[0];
+
+function myButton() {
+    modal.style.display = 'block';
+}
+
+function closeButton() {
+    modal.style.display = 'none';
+}
